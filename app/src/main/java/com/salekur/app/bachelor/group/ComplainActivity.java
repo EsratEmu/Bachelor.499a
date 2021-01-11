8812d4049d6ae4cc2bf387aa82fb9b23df8ad3cb
Salekur <salekur19@gmail.com>
2021-01-11 16:00:30 +0600

    Updated

---
+++ app/src/main/java/com/salekur/app/bachelor/group/ComplainActivity.java
@@ -0,0 +1,16 @@
+package com.salekur.app.bachelor.group;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.salekur.app.bachelor.R;
+
+public class ComplainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complain);
+    }
+}
